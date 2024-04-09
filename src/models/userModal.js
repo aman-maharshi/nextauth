75bb029fb75ec26e5d70c9423aa596f401dcac5f
aman-maharshi <amanm4harshi@gmail.com>
2024-04-09 12:21:59 +0530

    user data modeling update

--- src/models/userModal.js
+++ src/models/userModal.js
@@ -1,10 +1,34 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
-
-const userSchema  = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: [true, "Please provide a username"],
+    required: [true, "Please provide an username"],
     unique: true
   },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  isUserVerified: {
+    type: Boolean,
+    default: false
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
 })
+
+const User = mongoose.model.users || mongoose.model("users", userSchema)
+
+export default User
